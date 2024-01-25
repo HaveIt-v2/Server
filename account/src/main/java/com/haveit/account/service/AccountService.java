12b77ebe2a04965a189fb06944ee7a294ccf4c77
최혜원 <hwchoi33@naver.com>
2024-01-26 00:15:46 +0900

    ✨ feat: 회원 존재 및 탈퇴 여부 로직 생성, 재사용성 극대화

--- account/src/main/java/com/haveit/account/service/AccountService.java
+++ account/src/main/java/com/haveit/account/service/AccountService.java
@@ -1,13 +1,20 @@
 package com.haveit.account.service;
 
+import com.haveit.account.dto.request.LoginRequestDto;
 import com.haveit.account.dto.request.SignupRequestDto;
+import com.haveit.account.dto.response.LoginResponseDto;
+import com.haveit.account.dto.response.TokenResponseDto;
 import com.haveit.account.entity.Account;
 import com.haveit.account.enums.AccountType;
+import com.haveit.account.enums.MemberState;
 import com.haveit.account.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
@@ -29,4 +36,25 @@ public class AccountService {
         account = accountRepository.save(account);
         return account;
     }
+
+    public Account findMember(Object value){
+        Optional<Account> optionalAccount;
+
+        if(value instanceof Long){  //id값으로 확인
+            optionalAccount = accountRepository.findByMemberId((Long) value);
+        } else {  //email로 확인
+            optionalAccount = accountRepository.findByEmail((String) value);
+        }
+
+        if (optionalAccount.isEmpty()) {
+            throw new RuntimeException("존재하지 않는 회원입니다.");
+        } else if (optionalAccount.get().getState().equals(MemberState.DELETE.name())) {  //회원상태 확인 => delete 인지 확인
+            throw new RuntimeException("탈퇴한 회원입니다.");
+        }
+
+        return optionalAccount.get();
+    }
+
+    
+
 }
